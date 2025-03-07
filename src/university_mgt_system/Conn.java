5d0fa4efb6384c2c89d63e8f9a29654d75532b87
Baddest <davidjusu218@gmail.com>
2025-03-07 07:47:47 +0000

    Now switching to a local Mysql Workbench connection

--- src/university_mgt_system/Conn.java
+++ src/university_mgt_system/Conn.java
@@ -10,8 +10,8 @@ public class Conn {
     Conn () {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
-            c = DriverManager.getConnection("jdbc:mysql://umgtsys-db.cjiwygm4wndl.eu-north-1.rds.amazonaws.com:3306/umgtsys-db", "root", "davidjusu218");
-            //c = DriverManager.getConnection("jdbc:mysql:///employee", "root", "davidjusu218");
+            //c = DriverManager.getConnection("jdbc:mysql://umgtsys-db.cjiwygm4wndl.eu-north-1.rds.amazonaws.com:3306/umgtsys-db", "root", "davidjusu218");
+            c = DriverManager.getConnection("jdbc:mysql:///umgtsys_db", "root", "davidjusu218");
             s = c.createStatement();
             
         } catch (Exception e) {
